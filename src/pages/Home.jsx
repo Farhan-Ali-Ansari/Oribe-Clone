815860a2302a095371e825765370a6a1c798f91d
Farhan-Ali-Ansari <mefarhanafzal@gmail.com>
2025-10-30 21:13:13 +0500

    Feature and icons Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,14 @@ import "./Home.css";
 import hero1 from "../assets/hero1.jpg";
 import hero2 from "../assets/hero2.jpg";
 import hero3 from "../assets/hero3.jpg";
+import authenticity from "../assets/authenticity.png";
+import deluxe from "../assets/deluxe-samples.png";
+import shipping from "../assets/free-shipping.png";
+import gift from "../assets/gift-card.png";
+import featured1 from "../assets/featured1.jpg";
+import featured2 from "../assets/featured2.jpg";
+import featured3 from "../assets/featured3.jpg";
+import featured4 from "../assets/featured4.jpg";
 
 function Home() {
   const slides = [
@@ -67,6 +75,95 @@ function Home() {
         <button className="arrow right" onClick={nextSlide}>
           &gt;
         </button>
+      </section>
+      {/* --- FEATURES SECTION --- */}
+      <section className="icons-section">
+        <div className="icon">
+          <img src={authenticity} alt="Authenticity" />
+          <div className="icon-text">
+            <h4>AUTHENTICITY</h4>
+            <p>guaranteed</p>
+          </div>
+        </div>
+
+        <div className="icon">
+          <img src={deluxe} alt="Deluxe Samples" />
+          <div className="icon-text">
+            <h4>DELUXE SAMPLES</h4>
+            <p>available at checkout</p>
+          </div>
+        </div>
+
+        <div className="icon">
+          <img src={shipping} alt="Free Shipping" />
+          <div className="icon-text">
+            <h4>FREE SHIPPING</h4>
+            <p>with any $100+ order</p>
+          </div>
+        </div>
+
+        <div className="icon">
+          <img src={gift} alt="Gift Cards" />
+          <div className="icon-text">
+            <h4>DIGITAL</h4>
+            <p>gift cards</p>
+          </div>
+        </div>
+      </section>
+      {/* Before Featured Section */}
+      <section className="para">
+        <h1>
+          THESE ARE THE PRODUCTS OF <br /> THE HAIR OBSESSED
+        </h1>
+      </section>
+      {/* --- FEATURED PRODUCTS SECTION --- */}
+      <section className="featured-products">
+        <div className="featured-header">
+          <h2>FEATURED</h2>
+          <a href="#" className="view-all">
+            VIEW ALL
+          </a>
+        </div>
+
+        <div className="featured-grid">
+          <div className="featured-card">
+            <span className="featured-badge">New</span>
+            <img src={featured1} alt="Gold Lust Collection" />
+             <div className="featured-info">
+            <h4 className="featured-name">Gold Lust Collection</h4>
+            <p className="featured-price">$115.00</p>
+            </div>
+          </div>
+
+          <div className="featured-card">
+            <span className="featured-badge">New</span>
+            <img src={featured2} alt="Gold Lust Liter Set" />
+            <div className="featured-info">
+            <h4 className="featured-name">Gold Lust Liter Set</h4>
+            <p className="featured-price">$295.00</p>
+            </div>
+          </div>
+
+          <div className="featured-card">
+            <span className="featured-badge">New</span>
+            <img src={featured3} alt="Hair Alchemy Heatless Styling Set" />
+            <div className="featured-info">
+            <h4 className="featured-name">Hair Alchemy Heatless Styling Set</h4>
+            <p className="featured-price">$49.00</p>
+            </div>
+          </div>
+
+          <div className="featured-card">
+            <span className="featured-badge">New</span>
+            <img src={featured4} alt="Côte d’Azur Hand Wash and Crème Set" />
+            <div className="featured-info">
+            <h4 className="featured-name">
+              Côte d’Azur Hand Wash and Crème Set
+            </h4>
+            <p className="featured-price">$78.00</p>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
